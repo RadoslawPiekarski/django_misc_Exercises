4dc87a55f29f4ae1cb78f00561c7b07e18de70e3
Radek <radoslaw.piekarski@gmail.com>
2021-08-03 22:34:47 +0200

    added index view in challenges app

--- djangoExercises/urls.py
+++ djangoExercises/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from challenges.views import Challenges
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('challenges/', Challenges.as_view(), name="challenges")
 ]

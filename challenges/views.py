666fb87d9697d4745314df257d8ecec0eaee5f82
Radek <radoslaw.piekarski@gmail.com>
2021-08-04 00:10:41 +0200

    added monthly_challenge view in challenges app

--- challenges/views.py
+++ challenges/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
-def january(request):
-    return HttpResponse("Eat more protein entire month")
 
-def february(request):
-    return HttpResponse('Sleep at least 7h every day')
+def monthly_challenge(request, month):
+    if month == 'january':
+        challenge_text = 'Eat more protein entire month'
+    elif month == 'february':
+        challenge_text = 'Sleep at least 7h every day'
+    elif month == 'march':
+        challenge_text = 'Learn Django at least 15 minutes every day'
+    else:
+        return HttpResponseNotFound('This month is not supported!')
+    return HttpResponse(challenge_text)

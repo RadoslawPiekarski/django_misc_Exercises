7f3e4e52da88fc05b39cedb1d72311294e36a55e
Radek <radoslaw.piekarski@gmail.com>
2021-08-05 14:11:29 +0200

    added monthly_challenge dict

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('january', views.january),
-    path('february', views.february),
-    path("<month>", views.monthly_challenge)
+    path("<month>", views.monthly_challenge),
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
+
+monthly_challenges = {
+    'january': 'Eat more protein entire month',
+    'february': 'Sleep at least 7h every day',
+    'march': 'Learn Django at least 15 minutes every day',
+    'april': 'Eat more protein entire month',
+    'may': 'Sleep at least 7h every day',
+    'june': 'Learn Django at least 15 minutes every day',
+    'july': 'Eat more protein entire month',
+    'august': 'Sleep at least 7h every day',
+    'september': 'Learn Django at least 15 minutes every day',
+    'october': 'Eat more protein entire month',
+    'november': 'Sleep at least 7h every day',
+    'december': 'Learn Django at least 15 minutes every day',
+}
 # Create your views here.
 
 
